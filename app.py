d503a25dac144f3c50889b22ce45cfef367f1943
Olawale Akinseye <brainyfarm@gmail.com>
2017-12-05 17:11:38 +0100

    modify routes and nav items

--- app.py
+++ app.py
@@ -6,12 +6,17 @@ API_KEY = 'AIzaSyC-untCAlzyRtrAuJ6ShicN0aHCHMD94jg'
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home_page():
+    return 'Nothing Here for now'
+
+@app.route('/gym')
+def gym_page():
     return render_template('index.html', title='Gym Finder')
 
 
-@app.route('/find', methods=["GET", "POST"])
+@app.route('/gym/find', methods=["GET", "POST"])
 def find_gyms():
     if request.method == 'POST':
         location = request.form["location"]
@@ -24,7 +29,7 @@ def find_gyms():
         places_response = requests.get(places_search_url).json()
         return render_template('gyms.html', gyms=places_response)
 
-@app.route('/more/<place_id>', methods=["GET", "POST"])
+@app.route('/gym/more/<place_id>', methods=["GET", "POST"])
 def gym_info(place_id):
     if request.method == 'GET':
         place_api_url = 'https://maps.googleapis.com/maps/api/place/details/json?placeid=' + place_id + '&key=AIzaSyDHHLWzJzlZZFDye9JbxiCu4RXei_bzMbE'
