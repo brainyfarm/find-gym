44cfe8c6c0b36708997ca54bb37edc58101d42ed
Olawale Akinseye <brainyfarm@gmail.com>
2017-12-14 07:12:00 +0100

    add database

--- app.py
+++ app.py
@@ -1,7 +1,23 @@
 import requests
-from flask import Flask, render_template, url_for, request
+import pyrebase
+from hashids import Hashids
+from flask import Flask, render_template, url_for, request, redirect
+
+hashid_salt = 'impossible to guess'
+hashids = Hashids(salt=hashid_salt, min_length=4)
+
 
 API_KEY = 'AIzaSyC-untCAlzyRtrAuJ6ShicN0aHCHMD94jg'
+
+firebase_config = {
+  "apiKey": "AIzaSyAJGhs8jr8RTdUYtKGDx0dwAZ_03MySDH0",
+  "authDomain": "gym-finder-1512123683114.firebaseapp.com",
+  "databaseURL": "https://gym-finder-1512123683114.firebaseio.com",
+  "storageBucket": "gym-finder-1512123683114.appspot.com"
+}
+
+firebase = pyrebase.initialize_app(firebase_config)
+db = firebase.database()
 
 app = Flask(__name__)
 
@@ -12,7 +28,6 @@ def home_page():
 @app.route('/gym')
 def gym_page():
     return render_template('index.html')
-
 
 @app.route('/gym/find', methods=["POST"])
 def find_gyms():
@@ -31,16 +46,39 @@ def find_gyms():
 def gym_info(place_id):
     if request.method == 'GET':
         place_api_url = 'https://maps.googleapis.com/maps/api/place/details/json?placeid=' + place_id + '&key=AIzaSyDHHLWzJzlZZFDye9JbxiCu4RXei_bzMbE'
-        print place_api_url
         place_details_response = requests.get(place_api_url).json()
         return render_template('gym.html', gym=place_details_response)
     if request.method == 'POST':
+        new_booking_id = None
+        try:
+            bookings = db.child("bookings").get().val()
+            booking_id = [entry for entry in bookings][-1]['id']
+            new_booking_id = int(booking_id) + 1
+        except:
+            new_booking_id = 0
+        
+        new_booking_ref = db.child("bookings").child(new_booking_id)
+        booking_id_string = hashids.encode(new_booking_id)
+        booking_confirm_url = 'session/confirm/' + booking_id_string
         booking_details = dict()
+        booking_details['id'] = int(new_booking_id)
         booking_details['name'] = request.form["name"]
         booking_details['phone_number'] = request.form["phone"]
         booking_details['email'] = request.form['email']
         booking_details['date_time'] = request.form['date_time']
-        return render_template('confirm.html', booking_details=booking_details)
+        booking_details['id_string'] = booking_id_string
+        new_booking_ref.set(booking_details)
+        return redirect(booking_confirm_url)
+
+
+@app.route('/session/confirm/<booking_id>')       
+def display_confirmation(booking_id):
+    real_booking_id = hashids.decode(booking_id)[0]
+    booking_ref = db.child('bookings').child(real_booking_id)
+    result = booking_ref.get().val()
+    booking_details = dict(result)
+    return render_template('confirm.html', booking_details=booking_details)
+
 
 if __name__ == "__main__":
     app.run()
